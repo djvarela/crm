5bb7d92d3ed53ad1889f8a0d6f9987f9c3ef2447
Diego J. Varela <Djvarela89@gmail.com>
2024-02-05 00:42:37 -0300

    Ajuste del css form de loginpage

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,6 +1,7 @@
 import { useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import { AuthContext } from "../auth/context/AuthContext";
+import "../assets/css/normalize.css";
 import "../assets/css/login.css";
 
 export const LoginPage = () => {

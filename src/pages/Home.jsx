5f2b92746deb7b4d1551e4928e3aef8fa5d43382
Diego Varela <djvarela.fep@gmail.com>
2024-07-08 17:25:40 -0300

    Correcion de contador de lead ingresados en el dia

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,26 +1,35 @@
-
 export const Home = () => {
+  const totalLeads = JSON.parse(localStorage.getItem("leads")) || [];
+  const totalClients = JSON.parse(localStorage.getItem("customers")) || [];
+
+;
+
+  const nowLeads = totalLeads.map((lead) => {
+    let data = new Date(lead.date);
+
+    if (data.getDate() == new Date().getDate()) {
+      return lead;
+    }
+    
+  });
 
 
 
- const totalLeads = JSON.parse (localStorage.getItem('leads')) || [];
- const totalClients = JSON.parse (localStorage.getItem('customers')) || [];
-console.log(totalClients);
   return (
     <section className="home">
-
       <div className="home-status">
         <ul>
-          <li className="news-leads"><span>0</span> New leads</li>
-          <li><span>{totalLeads.length}</span> Total leads</li>
-          <li><span>{totalClients.length}</span> Total client</li> 
-      
-
+          <li className="news-leads">
+            <span>{nowLeads.length}</span> New leads
+          </li>
+          <li>
+            <span>{totalLeads.length}</span> Total leads
+          </li>
+          <li>
+            <span>{totalClients.length}</span> Total client
+          </li>
         </ul>
       </div>
-
     </section>
-  )
-}
-
-
+  );
+};
